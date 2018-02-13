9880c438cdef68d9f87c8fbfd100b0cb6df6ea23
NuclearCoder <hd85.ms@gmail.com>
2018-02-13 11:00:21 +0100

    Added overflow checks on native integer operations

--- natives/mathi.c
+++ natives/mathi.c
@@ -1,21 +1,74 @@
+#include <limits.h>
+
 #include "nat.h"
 
+
+#define ADD_OVERFLOW(a, b, min, max) \
+    ((b) < 0                        \
+     ? (a) < (min) - (b)            \
+     : (max) - (b) < (a))
+
+#define SUB_OVERFLOW(a, b, min, max) \
+    ((b) < 0                        \
+     ? (max) + (b) < (a)            \
+     : (a) < (min) + (b))           \
+
+#define MUL_OVERFLOW(a, b, min, max) \
+    ((b) < 0                        \
+     ? ((a) < 0                     \
+        ? (a) < (max) / (b)         \
+        : (b) == -1                 \
+        ? 0                         \
+        : (min) / (b) < (a))        \
+     : (b) == 0                     \
+     ? 0                            \
+     : ((a) < 0                     \
+        ? (a) < (min) / (b)         \
+        : (max) / (b) < (a)))
+
+#define DIV_OVERFLOW(a, b, min, max) \
+    ((min) < 0 && (b) == -1 && (a) < - (max))
+
+
 NATFUN2(addi, x, y) {
-    RINT(x->valInt + y->valInt);
+    int valx = x->valInt;
+    int valy = y->valInt;
+    if (ADD_OVERFLOW(valx, valy, INT_MIN, INT_MAX)) {
+        fprintf(stderr, "Integer add overflow\n");
+    }
+    RINT(valx + valy);
 }
 
 NATFUN2(subi, x, y) {
-    RINT(x->valInt - y->valInt);
+    int valx = x->valInt;
+    int valy = y->valInt;
+    if (SUB_OVERFLOW(valx, valy, INT_MIN, INT_MAX)) {
+        fprintf(stderr, "Integer subtract overflow\n");
+    }
+    RINT(valx - valy);
 }
 
 NATFUN2(muli, x, y) {
-    RINT(x->valInt * y->valInt);
+    int valx = x->valInt;
+    int valy = y->valInt;
+    if (MUL_OVERFLOW(valx, valy, INT_MIN, INT_MAX)) {
+        fprintf(stderr, "Integer multiply overflow\n");
+    }
+    RINT(valx * valy);
 }
 
 NATFUN2(divi, x, y) {
-    if (y->valInt == 0) {
+    int valx = x->valInt;
+    int valy = y->valInt;
+    if (valy == 0) {
         fprintf(stderr, "Division by zero\n");
         RUNIT();
     }
-    RINT(x->valInt / y->valInt);
+    if (DIV_OVERFLOW(valx, valy, INT_MIN, INT_MAX)) {
+        fprintf(stderr, "Integer division overflow\n");
+    }
+    RINT(valx / valy);
 }
+
+
+
