8f13554e937dc89b2d26d5764a87d5c86e971089
NuclearCoder <hd85.ms@gmail.com>
2018-03-21 10:38:18 +0100

    Added some formatting to the AST output

--- codegen.c
+++ codegen.c
@@ -2,141 +2,197 @@
 
 #include "codegen_main.xxd"
 
-#define gprintln()          fputs("\n", fout)
-#define gprint(line)        fputs(line "\n", fout)
-#define gprintf(line, ...)  fprintf(fout, line "\n", __VA_ARGS__)
-#define gprintast(field)     codegen_def_ast(fout, field )
-#define gprintastlist(field) codegen_def_astlist(fout, field )
-#define gprintnames(field)   codegen_def_names(fout, field )
 
-void codegen_def_astlist(FILE *fout, AstList *astlist);
-void codegen_def_ast(FILE *fout, Ast *ast);
-void codegen_def_names(FILE *fout, NameList *names);
+#define code_line()            fputc('\n', fout)
+#define code_print(str)        fputs(str, fout)
+#define code_println(str)      fputs(str "\n", fout)
+#define code_printf(str, ...)  fprintf(fout, str, __VA_ARGS__)
 
-void generate_code(FILE *fout, AstList *astlist) {
+void code_indent(void);
+#define with_indent(call)  do { code_indent(); call; } while (false)
+
+#define code_iprint(str)        with_indent(code_print(str))
+#define code_iprintln(str)      with_indent(code_println(str))
+#define code_iprintf(str, ...)  with_indent(code_printf(str, __VA_ARGS__))
+
+
+void gen_list(AstList *astlist);
+void gen_ast(Ast *ast);
+void gen_names(NameList *names);
+
+
+static int indent = 0;
+static FILE *fout = NULL;
+
+
+void generate_code(FILE *p_fout, AstList *astlist) {
+    fout = p_fout;
+    indent = 0;
 
     fwrite(codegen_main_pre, sizeof(char), codegen_main_pre_len, fout);
-    gprintln();
-    gprintln();
+    code_line();
+    code_line();
     
-    gprint("int main(void) {");
+    code_iprintln("int main(void) {");
+    ++indent;
 
-    gprint("/* Initialize the name table */");
-    gprint("names_init();");
-    gprintln();
+    code_iprintln("/* Initialize the name table */");
+    code_iprintln("names_init();");
+    
+    code_line();
 
-    gprint("/* Define the program statically */");
-    gprint("AstList *prog = ");
-    codegen_def_astlist(fout, astlist);
-    gprint(";");
-    gprintln();
+    code_iprintln("/* Define the program statically */");
+    code_iprint("AstList *prog = ");
+    gen_list(astlist);
+    code_println(";");
+    
+    code_line();
 
-    gprint("/* Execute the program */");
-    gprint("run_list(prog);");
-    gprintln();
+    code_iprintln("/* Execute the program */");
+    code_iprintln("run_list(prog);");
 
-    gprint("/* Free resources */");
-    gprint("alist_free(prog);");
-    gprint("names_free();");
+    code_line();
 
-    gprint("}");
-    gprintln();
+    code_iprintln("/* Free resources */");
+    code_iprintln("alist_free(prog);");
+    code_iprintln("names_free();");
+    
+    --indent;
+    code_iprintln("}");
+    code_line();
+
+    fout = NULL;
 
 }
 
-void codegen_def_astlist(FILE *fout, AstList *astlist) {
+void gen_list(AstList *astlist) {
     if (astlist == NULL) {
-        gprint("((AstList *) 0)");
+        code_print("0");
         return;
     }
 
-    gprint("((AstList *) list_from(");
-    
+    code_println("list_from(");
+    ++indent;
+
     // generate one definition for each
     ListIterator it;
     list_iterate(&astlist, &it);
     
     while (list_iter_has_more(&it)) {
         Ast *ast = list_iter_next(&it);
-       
-        codegen_def_ast(fout, ast);
-        gprint(",");
+      
+        code_indent();
+        gen_ast(ast);
+        code_println(",");
     }
+    code_iprintln("0");
 
-    gprint("0))");
+    --indent;
+    code_iprint(")");
 }
 
 
-void codegen_def_ast(FILE *fout, Ast *ast) {
+void gen_ast(Ast *ast) {
     if (ast == NULL) {
-        gprint("0");
+        code_print("0");
         return;
     }
 
     switch (ast->type) {
     case e_unit:
-        gprint("ast_make_unit()");
+        code_print("ast_make_unit()");
         break;
     case e_int:
-        gprintf("ast_make_integer(%d)", ast->exprInteger);
+        code_printf("ast_make_integer(%d)", ast->exprInteger);
         break;
     case e_float:
-        gprintf("ast_make_float(%fF)", ast->exprFloat);
+        code_printf("ast_make_float(%fF)", ast->exprFloat);
         break;
     case e_bool:
-        gprintf("ast_make_bool(%d)", ast->exprBool);
+        code_printf("ast_make_bool(%d)", ast->exprBool);
         break;
     case e_string:
         /* The string attribute already is escaped, with quotes.
          * Duplicate it because ast_free assumes it is malloc-ed. */
-        gprintf("ast_make_string(strdup(%s))", ast->exprString);
+        code_printf("ast_make_string(strdup(%s))", ast->exprString);
         break;
     case e_var:
         /* Rebuild the name table at the same time. */
-        gprintf("ast_make_variable(names_getid(\"%s\"))",
+        code_printf("ast_make_variable(names_getid(\"%s\"))",
                 names_getnm(ast->exprVariable));
         break;
     case e_block:
-        gprint("ast_make_block(");
-        gprintast(ast->exprBlock);
-        gprint(")");
+        code_println("ast_make_block(");
+        ++indent;
+        code_indent();
+        gen_ast(ast->exprBlock);
+        --indent;
+        code_line();
+        code_iprint(")");
         break;
     case e_list:
-        gprint("ast_make_list(");
-        gprintastlist(ast->exprList);
-        gprint(")");
+        code_println("ast_make_list(");
+        ++indent;
+        code_indent();
+        gen_list(ast->exprList);
+        --indent;
+        code_line();
+        code_iprint(")");
         break;
     case e_funcall:
-        gprint("ast_make_funcall(");
-        gprintast(ast->exprFunCall.function);
-        gprint(",");
-        gprintastlist(ast->exprFunCall.args);
-        gprint(")");
+        code_println("ast_make_funcall(");
+        ++indent;
+        code_indent();
+        gen_ast(ast->exprFunCall.function);
+        code_println(",");
+        code_indent();
+        gen_list(ast->exprFunCall.args);
+        --indent;
+        code_line();
+        code_iprint(")");
         break;
     case e_let:
-        gprint("ast_make_let(");
-        gprintnames(ast->exprLet.names);
-        gprintf(", %d,", ast->exprLet.rec);
-        gprintnames(ast->exprLet.params);
-        gprint(",");
-        gprintast(ast->exprLet.expr);
-        gprint(",");
-        gprintast(ast->exprLet.block);
-        gprint(")");
+        code_println("ast_make_let(");
+        ++indent;
+        code_indent();
+        gen_names(ast->exprLet.names);
+        code_println(",");
+        code_iprintf("%d,\n", ast->exprLet.rec);
+        code_indent();
+        gen_names(ast->exprLet.params);
+        code_println(",");
+        code_indent();
+        gen_ast(ast->exprLet.expr);
+        code_println(",");
+        code_indent();
+        gen_ast(ast->exprLet.block);
+        --indent;
+        code_line();
+        code_iprint(")");
         break;
     case e_if:
-        gprint("ast_make_if(");
-        gprintast(ast->exprIf.cond);
-        gprint(",");
-        gprintast(ast->exprIf.bIf);
-        gprint(",");
-        gprintast(ast->exprIf.bElse);
-        gprint(")");
+        code_println("ast_make_if(");
+        ++indent;
+        code_indent();
+        gen_ast(ast->exprIf.cond);
+        code_println(",");
+        code_indent();
+        gen_ast(ast->exprIf.bIf);
+        code_println(",");
+        code_indent();
+        gen_ast(ast->exprIf.bElse);
+        --indent;
+        code_line();
+        code_iprint(")");
         break;
     case e_tuple:
-        gprint("ast_make_tuple(");
-        gprintastlist(ast->exprTuple);
-        gprint(")");
+        code_println("ast_make_tuple(");
+        ++indent;
+        code_indent();
+        gen_list(ast->exprTuple);
+        --indent;
+        code_line();
+        code_iprint(")");
         break;
     default:
         fprintf(stderr, "Codegen ast type not implemented yet\n");
@@ -144,13 +200,14 @@ void codegen_def_ast(FILE *fout, Ast *ast) {
     }
 }
 
-void codegen_def_names(FILE *fout, NameList *names) {
+void gen_names(NameList *names) {
     if (names == NULL) {
-        gprint("((NameList *) 0)");
+        code_print("0");
         return;
     }
 
-    gprint("((NameList *) list_from(");
+    code_println("list_from(");
+    ++indent;
     
     ListIterator it;
     list_iterate(&names, &it);
@@ -160,9 +217,35 @@ void codegen_def_names(FILE *fout, NameList *names) {
 
         /* Again, we redefine names. */
         const char *name = names_getnm(id);
+        
+        code_iprintf("nmalloc(names_getid(\"%s\")),\n", name);
+    }
+    code_iprintln("0");
 
-        gprintf("nmalloc(names_getid(\"%s\")),", name);
+    --indent;
+    code_iprint(")");
+}
+
+
+#define TAB  "   "
+#define PRETAB_COUNT 4        // exclusive
+
+void code_indent(void) {
+    static const char *indents[] = {
+        TAB,
+        TAB TAB,
+        TAB TAB TAB,
+        TAB TAB TAB TAB
+    };
+   
+    int nb = indent;
+
+    while (nb > PRETAB_COUNT) {
+        fputs(indents[PRETAB_COUNT - 1], fout);
+        nb -= PRETAB_COUNT;
     }
 
-    gprint("0))");
+    if (nb > 0) {
+        fputs(indents[nb - 1], fout);
+    }
 }
